e22cf5ee5460a8c482e3b97769dfb566318d04d5
Vivek Kumbhar <kumbharvivek93@gmail.com>
2026-07-02 23:49:03 +0530

    Add files via upload

---
+++ pandas/datacleaning.py
@@ -0,0 +1,7 @@
+# import pandas as pd
+# df=pd.read_csv('C:/Users/vivek kumbhar/Desktop/machine learning/regdatacleaned.csv')
+# print(df.to_string())
+
+import pandas as pd 
+df=pd.read_csv(r'C:\Users\vivek kumbhar\Desktop\pandas\shop_smart_ecommerce(1).csv')
+print(df.to_string())
